4103aea3cc9c0ba02441addc03a0223b475f2a08
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2017-04-01 02:03:13 +0700

    added profile toko sementara

--- app/src/main/java/com/example/arifinfirdaus/cato/FavoritePlaceDetailActivity.java
+++ app/src/main/java/com/example/arifinfirdaus/cato/FavoritePlaceDetailActivity.java
@@ -2,14 +2,28 @@ package com.example.arifinfirdaus.cato;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.arifinfirdaus.cato.dummy.DummyContent;
+
+import java.util.List;
 
 /**
  * An activity representing a single Item detail screen. This
@@ -18,6 +32,8 @@ import android.view.MenuItem;
  * in a {@link FavoritePlaceListActivity}.
  */
 public class FavoritePlaceDetailActivity extends AppCompatActivity {
+
+    private ImageView ivGambarBarang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +78,43 @@ public class FavoritePlaceDetailActivity extends AppCompatActivity {
                     .add(R.id.item_detail_container, fragment)
                     .commit();
         }
+
+
+        View recyclerView = findViewById(R.id.rv_gambar_barang_penjual);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+        // hitung lebar screen
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+//        ivGambarBarang = (ImageView) findViewById(R.id.iv_gambar_barang);
+////        ivGambarBarang.getLayoutParams().width = width / 3;
+////        ivGambarBarang.getLayoutParams().height = width / 3;
+//
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(100, 100);
+//        ivGambarBarang.setLayoutParams(layoutParams);
+
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new FavoritePlaceDetailActivity.SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+
+//        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+//        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setNestedScrollingEnabled(false);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile_toko, menu);
+
+        return true;
     }
 
     @Override
@@ -78,6 +131,86 @@ public class FavoritePlaceDetailActivity extends AppCompatActivity {
             NavUtils.navigateUpTo(this, new Intent(this, FavoritePlaceListActivity.class));
             return true;
         }
+
+        if (id == R.id.action_favorite) {
+            Toast.makeText(this, "Toko ini masuk daftar favorit Anda", Toast.LENGTH_SHORT).show();
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<FavoritePlaceDetailActivity.SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<DummyContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public FavoritePlaceDetailActivity.SimpleItemRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.gambar_barang_list_content, parent, false);
+            return new FavoritePlaceDetailActivity.SimpleItemRecyclerViewAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final FavoritePlaceDetailActivity.SimpleItemRecyclerViewAdapter.ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            // holder.mIdView.setText("Kategori " + mValues.get(position).id);
+            // holder.mContentView.setText(mValues.get(position).content);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    if (mTwoPane) {
+//                        Bundle arguments = new Bundle();
+//                        arguments.putString(CategoriesDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//                        CategoriesDetailFragment fragment = new CategoriesDetailFragment();
+//                        fragment.setArguments(arguments);
+//                        getSupportFragmentManager().beginTransaction()
+//                                .replace(R.id.item_detail_container, fragment)
+//                                .commit();
+//                    } else {
+//                        Context context = v.getContext();
+//                        Intent intent = new Intent(context, CategoriesDetailActivity.class);
+//                        intent.putExtra(CategoriesDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//
+//                        context.startActivity(intent);
+//                    }
+
+//                    Context context = v.getContext();
+//                    Intent intent = new Intent(context, CategoriesDetailActivity.class);
+//                    intent.putExtra(CategoriesDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+//
+//                    context.startActivity(intent);
+
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final ImageView mImageView;
+            // public final TextView mContentView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mImageView = (ImageView) view.findViewById(R.id.iv_gambar_barang);
+                // mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString();
+            }
+        }
     }
 }
